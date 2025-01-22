66a7017a57abad61693cf5717e9ad62d6d14250f
qinglongcn <1435293080@qq.com>
2025-01-22 15:21:28 +0800

    Delete p2p directory

--- p2p/host/basic/mocks.go
+++
@@ -1,8 +0,0 @@
-//go:build gomock || generate
-
-package basichost
-
-// 生成 NAT 接口的 mock 实现,用于测试
-//
-//go:generate sh -c "go run go.uber.org/mock/mockgen -build_flags=\"-tags=gomock\" -package basichost -destination mock_nat_test.go github.com/dep2p/p2p/host/basic NAT"
-type NAT nat
